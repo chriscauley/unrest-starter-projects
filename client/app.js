a5bb3f28c6e0bdb70b76385d7eb71c41eb9566c4
chriscauley <chris@lablackey.com>
2019-04-05 16:37:06 +0000

    home screen and starter route

--- client/app.js
+++ client/app.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+import uR from 'unrest.io'
+import models from './models'
+
+import './tags'
+import './routes'
+
+uR.auth.enabled = false // uses a dummy super user
+
+uR.ready(() => {
+  uR.admin.start()
+})
